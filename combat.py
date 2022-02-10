b068db78ca5a18d29e70f7058a3429f33383abd3
muuli2022 <96959556+muuli2022@users.noreply.github.com>
2022-02-10 21:28:00 +0200

    Add files via upload
    version 0.3 multicombat

--- combat.py
+++ combat.py
@@ -14,7 +14,8 @@ class Opponent:
     W: int = 7
     I: int = 40
     A: int = 2
-    parry: str = "off"
+    parry: bool = False
+    sudden_death: bool = False
 
     def throwDamage(self):
         a = random.randint(1, 6)
@@ -40,17 +41,8 @@ class Opponent:
 
         return damage + self.S
 
-
-@bind(document["set_attributes"], "click")
-def setAttributes(ev):
-
-    setAttributesOf(attacker, "a")
-    setAttributesOf(defender, "d")
-
-    docOutput("Attributes set")
-
 @bind(document["set_Bertold"], "click")
-def setAttributes(ev):
+def setBertold(ev):
 
     document['n_d'].value = "Bertold"
     document['ws_d'].value = "69"
@@ -60,10 +52,12 @@ def setAttributes(ev):
     document['i_d'].value = "63"
     document['a_d'].value = "4"
  
+    setAttributesOf(defender, "d")
+
     docOutput("Bertold set")
 
 @bind(document["set_Michael"], "click")
-def setAttributes(ev):
+def setMichael(ev):
 
     document['n_d'].value = "Michael"
     document['ws_d'].value = "79"
@@ -73,7 +67,64 @@ def setAttributes(ev):
     document['i_d'].value = "58"
     document['a_d'].value = "4"
 
+    setAttributesOf(defender, "d")
+
     docOutput("Michael set")
+
+# @bind(document["test"], "click")
+# def test(ev):
+
+#     if document['inout_a1'].checked:
+#         document['inout_a1'].checked = False
+#     else:
+#         document['inout_a1'].checked = True
+
+@bind(document["round"], "click")
+def round(ev):
+    
+    setAttributesOf(defender, "d")
+   
+
+    defender_initiatives = cf.calculateInitiatives(defender.I, defender.A)
+
+    hitlist = getHitlist(attackers)    
+
+    hitlist = cf.createHitList(hitlist, defender_initiatives, "d")
+
+    hitlist.sort(reverse=True)
+
+
+    defenders = ["d"]
+    combatants = attackers + defenders
+    
+
+    hitlist = updateRoundStatusToHitlist(hitlist, combatants)    
+
+    document["output"] <= P(B(runCombatRound(hitlist)))
+
+    document["output"].scrollTop = document["output"].scrollHeight
+
+@bind(document["sudden_death"], "click")
+def suddenDeath(ev):
+    attacker.sudden_death = document["sudden_death"].checked
+
+
+@bind(document["parry_d"], "click")
+def defenderParry(ev):
+    defender.parry = document["parry_d"].checked
+
+def addAttackerInOutHandler(i):
+    @bind(document[f"inout_a{i}"], "click")
+    def attackerInOut(ev):
+        
+        if attackers.count(f"a{i}") == 0:
+            attackers.append(f"a{i}")
+        else:
+            attackers.remove(f"a{i}")
+
+
+for i in range(1, 5):
+    addAttackerInOutHandler(i)
 
 def docOutput(string, styling=None):
     
@@ -93,191 +144,268 @@ def setAttributesOf(opponent, suffix):
     opponent.W = int(document['w_' + suffix].value)
     opponent.I = int(document['i_' + suffix].value)
     opponent.A = int(document['a_' + suffix].value)
+    
+    if suffix != "d":
+        opponent.parry = document['parry_' + suffix].checked
 
-def runCombatRound(att_inits, def_inits, attacks):
+
+def getHitlist(attackers):
+
+    hitlist = []
     i = 0
-    ai = 0
-    di = 0
+    while i < len(attackers):
 
+        temp_initiative = cf.calculateInitiatives(int(document['i_' + attackers[i]].value), int(document['a_' + attackers[i]].value))
+        hitlist = cf.createHitList(hitlist, temp_initiative, attackers[i])
+        i += 1
+    
+    hitlist.sort(reverse=True)
+    
+    return hitlist
+
+
+def saveRoundStatus(hitlist, attackers, isend):
+
+    defenders = ["d"]
+
+    combatants = attackers + defenders
+
+    if isend == 0:
+        i = 0
+
+        while i < len(combatants):
+            k = 0
+            temp = 0
+            while k < len(hitlist):
+                if hitlist[k][1] == combatants[i]:
+                    temp = temp + hitlist[k][2]
+                k += 1
+            document['ua_' + combatants[i]].value = str(temp)
+            i += 1
+    else:
+
+        i = 1
+
+        while i < 5:
+            document['ua_' + 'a' + str(i)].value  = "0"
+            
+            i += 1
+
+        document['ua_d'].value  = "0"
+
+    return
+
+def updateRoundStatusToHitlist(hitlist, combatants):
+
+    i = 0
+
+    while i < len(combatants):
+
+        k = 0
+        j = 0
+        while k < len(hitlist):
+            
+            if hitlist[k][1] == combatants[i]:
+                if j == int(document['ua_' + combatants[i]].value):
+                    break
+                hitlist[k][2] = 1
+                j += 1
+
+            k += 1
+
+        i += 1
+  
+    return hitlist      
+
+def runCombatRound(hitlist):
+    i = 0
+    attacks = len(hitlist)
+ 
     message = ""
 
     while i < attacks:
 
-        if att_inits[ai] > def_inits[di]:
-            
-            message = attacker.name + " hits on initative " + str(att_inits[ai])
-            docOutput(message)
-            
-            ai += 1
- 
-            hit = random.randint(1,100)
-
-            message = attacker.name + " threw " + str(hit)
-            docOutput(message)
-
-            if hit <= attacker.WS:
-
-                message = attacker.name + " hits " + str(cf.findHitLocation(hit))
+        if hitlist[i][3] == "a":
+            setAttributesOf(attacker, hitlist[i][1])
+            if hitlist[i][2] == 0:
+                message = attacker.name + " hits on initiative " + str(hitlist[i][0])
                 docOutput(message)
                 
-                #docOutput("paikka a.0.5")
-
-                damage = attacker.throwDamage()
-
-                parry_outcome = 0
-
-                if defender.parry == "on":
-                    parry_outcome = cf.parryResult(di,defender.WS, defender.A)
-                    di += 1
-                    attacks -= 1
-
-                    if parry_outcome > 0:
-                        message = defender.name + " parried " + str(parry_outcome) + " points of damage."
-                        docOutput(message)
-                    else:
-                        message = defender.name + "'s parry was unsuccesful."
-                        docOutput(message)
-
-                #docOutput("paikka a.2")
-
-                outcome = cf.findHitOutcome(damage, defender.T, parry_outcome) 
-
-                #docOutput("paikka a.3")
-
-                message = attacker.name + " did " + str(damage) + " points damage and " + defender.name + " lost " + str(outcome) + " wounds"
+                hitlist = cf.markAttackUsed(hitlist, hitlist[i][1], i, 0)
+                
+                hit = random.randint(1,100)
+                
+                message = attacker.name + " threw " + str(hit)
                 docOutput(message)
-                defender.W -= outcome
 
-                if defender.W < 0:
-                    temp = str(defender.W)
+                if hit <= attacker.WS:
 
-                    criticalHit = "+" + temp[1:]
-                    defender.W = 0
-
-                    return defender.name + " suffers " + criticalHit + " critical hit. Throw a critical effect!"
-                else:
-                    message = defender.name + " has " + str(defender.W) + " wounds left."
+                    message = attacker.name + " hits " + str(cf.findHitLocation(hit))
                     docOutput(message)
-
-            else:
-                
-                if cf.checkFumble(hit) == "Miss!":
-                    docOutput("Miss!")
-                else:
-                    message = attacker.name + " fumbled with roll " + str(hit) + ". Loose one attack."
-                    docOutput(message, "bold")
-                    if ai < len(att_inits) - 1:
-                        ai +=1
-                        attacks -= 1
-        else:
-            message = defender.name + " hits on initative " + str(def_inits[di])
-            docOutput(message)
-            
-            di += 1
- 
-            hit = random.randint(1,100)
-
-            message = defender.name + " threw " + str(hit)
-            docOutput(message)
-
-            if hit <= defender.WS:
-                parry_outcome = 0
-                message = defender.name + " hits " + str(cf.findHitLocation(hit))
-                docOutput(message)
-                #docOutput("paikka d.0.5")
-                damage = defender.throwDamage()
-
-                parry_outcome = 0
-
-                if attacker.parry == "on":
-                    parry_outcome = cf.parryResult(ai,attacker.WS, attacker.A)
-                    ai += 1
-                    attacks -= 1
-
-                    if parry_outcome > 0:
-                        message = attacker.name + " parried " + str(parry_outcome) + " points of damage."
-                        docOutput(message)
-                    else:
-                        message = attacker.name + "'s parry was unsuccesful."
-                        docOutput(message)
-
-
-
-                #docOutput("paikka d.1")
-                outcome = cf.findHitOutcome(damage, attacker.T, parry_outcome)
-                #docOutput("paikka d.2")
-                message = defender.name + " did " + str(damage) + " points damage and " + attacker.name + " lost " + str(outcome) + " wounds"
-                #docOutput("paikka d.3")
-                
-                docOutput(message)
-                attacker.W -= outcome
-
-                if attacker.W < 0:
                     
-                    temp = str(attacker.W)
+                    damage = attacker.throwDamage()
 
-                    criticalHit = "+" + temp[1:]
-                    attacker.W = 0
+                    parry_outcome = 0
 
-                    return attacker.name + " suffers " + criticalHit + " critical hit. Throw a critical effect!"
-                     
-                else:
-                    message = attacker.name + " has " + str(attacker.W) + " wounds left."
+                    if defender.parry:
+                        parry_outcome = cf.parryResult(hitlist, "d", i, defender.WS)
+                        
+                        hitlist = cf.markAttackUsed(hitlist, "d", i, 0)
+                        
+                        if parry_outcome > 0:
+                            message = defender.name + " parried " + str(parry_outcome) + " points of damage."
+                            docOutput(message)
+                        else:
+                            message = defender.name + "'s parry was unsuccesful."
+                            docOutput(message)
+
+                    outcome = cf.findHitOutcome(damage, defender.T, parry_outcome) 
+
+                    message = attacker.name + " did " + str(damage) + " points damage and " + defender.name + " lost " + str(outcome) + " wounds"
                     docOutput(message)
+                    defender.W -= outcome
+
+                    document['w_d'].value = str(defender.W)
+
+                    if defender.W < 0:
+                        temp = str(defender.W)
+
+                        criticalHit = "+" + temp[1:]
+                        defender.W = 0
+                        document['w_d'].value = str(defender.W)
+
+                        message = defender.name + " suffers " + criticalHit + " critical hit. Throw a critical effect!"
+                        saveRoundStatus(hitlist, attackers, 0)
+                        return message
+                    else:
+                        message = defender.name + " has " + str(defender.W) + " wounds left."
+                        docOutput(message)
+
+                else:
+                    
+                    if cf.checkFumble(hit) == "Miss!":
+                        docOutput("Miss!")
+                    else:
+                        message = attacker.name + " fumbled with roll " + str(hit) + ". Loose one attack."
+                        docOutput(message, "bold")
+                        hitlist = cf.markAttackUsed(hitlist, hitlist[i][1], i, 1)
+               
+            else:
+                message = attacker.name + " has already used action on initiative " + str(hitlist[i][0])
+                docOutput(message)
+
+            i += 1
+        else:
+            setAttributesOf(defender, "d")
+            if hitlist[i][2] == 0:
+                message = defender.name + " hits on initiative " + str(hitlist[i][0])
+                docOutput(message)
+
+                hitlist = cf.markAttackUsed(hitlist, "d", i, 0)
+
+                setAttributesOf(attacker, attackers[0])
+
+                hit = random.randint(1,100)
+                
+                message = defender.name + " threw " + str(hit)
+                docOutput(message)
+
+                if hit <= defender.WS:
+                    message = defender.name + " hits " + str(cf.findHitLocation(hit))
+                    docOutput(message)
+
+                    damage = defender.throwDamage()
+
+                    parry_outcome = 0
+
+                    if attacker.parry:
+                        
+                        parry_outcome = cf.parryResult(hitlist, attackers[0], i, attacker.WS)
+                        
+                        hitlist = cf.markAttackUsed(hitlist, attackers[0], i, 0)
+
+                        if parry_outcome > 0:
+                            message = attacker.name + " parried " + str(parry_outcome) + " points of damage."
+                            docOutput(message)
+                        else:
+                            message = attacker.name + "'s parry was unsuccesful."
+                            docOutput(message)
+
+                    outcome = cf.findHitOutcome(damage, attacker.T, parry_outcome)
+
+                    message = defender.name + " did " + str(damage) + " points damage and " + attacker.name + " lost " + str(outcome) + " wounds"
+                    docOutput(message)
+
+                    attacker.W -= outcome
+
+                    document['w_' + attackers[0]].value = str(attacker.W)
+
+                    if attacker.W < 0:
+                        
+                        temp = str(attacker.W)
+
+                        criticalHit = "+" + temp[1:]
+                        attacker.W = 0
+                        document['w_' + attackers[0]].value = str(attacker.W)
+
+                        if attacker.sudden_death:
+
+                            critical_outcome = cf.checkSuddenDeath(int(temp[1:]), hit)
+
+                            if critical_outcome[0] == 1:
+
+                                message = defender.name + "'s" + critical_outcome[1]
+                                saveRoundStatus(hitlist, attackers, 0)
+
+                                document['inout_' + attackers[0]].checked = False
+
+                                attackers.remove(attackers[0])
+                                return message
+                            else:
+                                message = critical_outcome[1]
+                                docOutput(message, "bold")
+                        else:
+                        
+                            message = attacker.name + " suffers " + criticalHit + " critical hit. Throw a critical effect!"
+                            saveRoundStatus(hitlist, attackers, 0)
+
+                            return message
+                        
+                    else:
+                        message = attacker.name + " has " + str(attacker.W) + " wounds left."
+                        docOutput(message)
+
+                else:
+                    
+                    if cf.checkFumble(hit) == "Miss!":
+                        docOutput("Miss!")
+                    else:
+                        message = defender.name + " fumbled with roll " + str(hit) + ". Loose one attack."
+                        docOutput(message, "bold")
+                        hitlist = cf.markAttackUsed(hitlist, hitlist[i][1], i, 1)
 
             else:
-                
-                if cf.checkFumble(hit) == "Miss!":
-                    docOutput("Miss!")
-                else:
-                    message = defender.name + " fumbled with roll " + str(hit) + ". Loose one attack."
-                    docOutput(message)
-                    if di < len(def_inits) - 1:
-                        di +=1
-                        attacks -= 1
+                message = defender.name + " has already used action on initiative " + str(hitlist[i][0])
+                docOutput(message)
 
-        i += 1
+            i += 1
 
-    return "Attacks done"
 
-@bind(document["round"], "click")
-def round(ev):
+    saveRoundStatus(hitlist, attackers, 1)
     
-    defender_initiatives = cf.calculateInitiatives(defender.I, defender.A)
-    attacker_initiatives = cf.calculateInitiatives(attacker.I, attacker.A)
-
-    i = 0
-    all_attacks = attacker.A + defender.A
-    
+    return "Attacks done!"
 
 
-    defender_initiatives.append(0)
-    attacker_initiatives.append(0)
-
-    document["output"] <= P(B(runCombatRound(attacker_initiatives, defender_initiatives, all_attacks)))
-
-
-    document["output"].scrollTop = document["output"].scrollHeight
-
-@bind(document["parry_a"], "click")
-def checkboxtest(ev):
-    if attacker.parry == "off":
-        attacker.parry = "on"
-    else:
-        attacker.parry = "off"
-
-@bind(document["parry_d"], "click")
-def checkboxtest(ev):
-    if defender.parry == "off":
-        defender.parry = "on"
-    else:
-        defender.parry = "off"
 
 # Program running starts here
 
 defender = Opponent()
     
 attacker = Opponent()
+
+attackers = []
+
+
 
 
 
--- combatfunctions.py
+++ combatfunctions.py
@@ -67,16 +67,75 @@ def checkFumble(hit : int):
 
     return "Miss!"
 
-def parryResult(att_nbr, ws, attacks):
+def parryResult(hitlist, defender, start, ws):
 
     parry_dmg = 0
 
-    if att_nbr < attacks:
-        proll = random.randint(1,100)
+    i = start + 1
+
+    while i < len(hitlist):
+        if hitlist[i][1] == defender:
+            if hitlist[i][2] == 0:
+
+                proll = random.randint(1,100)
                     
-        if ws >= proll:
-            parry_dmg = random.randint(1,6)
-        else:
-            parry_dmg = 0
+                if ws >= proll:
+                    parry_dmg = random.randint(1,6)
+                else:
+                    parry_dmg = 0
+                
+                return parry_dmg
+        
+        i += 1
+
 
     return parry_dmg
+
+def createHitList(hitlist_current, initiative_list, name):
+
+    i = 0
+
+    while i < len(initiative_list):
+        
+        hitlist_current.append([initiative_list[i], name, 0, name[0]])
+
+        i += 1
+
+    return hitlist_current
+
+def markAttackUsed(hitlist, attacker, start, adjustment):
+    i = start + adjustment
+    while i < len(hitlist):
+        if hitlist[i][1] == attacker:
+            hitlist[i][2] = 1
+            return hitlist
+        i += 1
+    return hitlist
+
+def checkSuddenDeath(critical_hit, hit):
+
+    critical_ranges = [9, 20, 30, 40, 50, 100]
+    critical_outcomes = [[0,0,0,0,0,1], [0,0,0,0,1,1], [0,0,0,1,1,1], [0,0,1,1,1,1], [0,1,1,1,1,1], [1,1,1,1,1,1,1]]
+
+    critical_roll = random.randint(1,100)
+
+    if critical_hit > 6:
+        critical_hit = 6
+
+    i = 0
+
+    while True:
+
+        if critical_roll <= critical_ranges[i]:
+            critical_outcome = critical_outcomes[i][critical_hit - 1]
+            break
+        else:
+            i += 1
+
+    if critical_outcome == 1:
+        hit_location = findHitLocation(hit)
+        message = " blow smashes opponents " + hit_location + ". Death from shock and blood loss is almost instantaneous."
+        return [critical_outcome, message]
+    else:
+        message = "Your opponent is staggered, but recoveres quickly and continues the fight."
+        return [critical_outcome, message]
